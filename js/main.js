4f3953dcfee4a5576e45b539389833c6860f2f20
halison vitorino <halisonvitorino@gmail.com>
2021-09-15 15:30:31 -0300

    Ver 01

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//var nome = "Halison";
+//alert(nome + ": seja bem vindo!!!");
+
+/*
+var idade = prompt("Qual é a sua idade?")
+if(idade <= 18){
+    alert("Voce é menor de idade")
+} else {
+    alert("Voce é maior de idade")
+}
+*/
+/*
+var data = new(Date);
+alert("Estamos no mes " + data.getMonth());
+alert("Estamos na hora " + data.getHours());
+*/
+
+function soma (n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(10,10));
+
+/*var contador = 10;
+while(contador > 1){
+    console.log(contador);
+    contador = contador - 1;
+}
+*/
+
+
+//console.log(nome);
